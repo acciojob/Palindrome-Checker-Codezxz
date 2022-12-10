5302df6aa6113a06ed171bc55ccb81e882221edd
Nirnay N. Sheshware <107663236+Codezxz@users.noreply.github.com>
2022-12-10 13:05:07 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+  int n = str.legnth();
+  String rev="";
+  for(int i = n-1 ; i >= 0 ; i--){
+    rev = rev+n.charAt(i);
+  }
+  if(n.equals(rev)){
+    System.out.print("true");
+  }else{
+    System.out.print("false");
+  }
 }
 module.exports = palindrome
